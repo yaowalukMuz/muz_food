a3a028777bc692bafe115dba15b02518a029ef14
yaowalukMuz <yaowamuz.c@gmail.com>
2019-03-16 10:08:29 +0700

    Design UI Register success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,13 +6,52 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  Widget showTitle = Text('Please Register');
+
+  Widget nameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Name :', hintText: 'Name User'),
+    );
+  }
+
+  Widget emailTextField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Email :', hintText: 'Your Email Address'),
+    );
+  }
+
+  Widget passwordTextField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Password :', hintText: 'Your Passwod'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
+        actions: <Widget>[
+          IconButton(
+            tooltip: 'Upload to Server',
+            icon: Icon(Icons.cloud_upload),
+            onPressed: () {
+              print('Your Click Upload');
+            },
+          )
+        ],
       ),
-      body: Text('This is body'),
+      body: Container(
+          padding: EdgeInsets.only(top: 60.0),
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: Column(
+              children: <Widget>[showTitle, nameTextField(), emailTextField(),passwordTextField()],
+            ),
+          )),
     );
   }
 }
